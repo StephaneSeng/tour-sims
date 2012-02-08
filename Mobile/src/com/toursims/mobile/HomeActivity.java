37ebc3485682c4e1e9bf7603bd55372d87301c3c
Hugo Pastore <hpastore@kozea.fr>
2012-02-08 14:31:31 +0100

    list at the beginning

--- Mobile/src/com/toursims/mobile/HomeActivity.java
+++ Mobile/src/com/toursims/mobile/HomeActivity.java
@@ -1,13 +1,27 @@
 package com.toursims.mobile;
 
-import android.app.Activity;
+//import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_courses, COURSES));
+        
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        
+        
     }
+    
+    static final String[] COURSES = new String[] {
+    	"LaDoua", "INSA", "Lyon1", "IUT-Feyssine"
+    };
 }
+
---
+++ Mobile/src/com/toursims/mobile/TourSims.java
@@ -0,0 +1,7 @@
+package com.toursims.mobile;
+
+import android.app.Application;
+
+public class TourSims extends Application {
+
+}
