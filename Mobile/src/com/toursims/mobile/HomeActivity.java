1904e17f4c428d55eff25f6d94f55ec039735815
Stéphane Seng <seng.stephane@gmail.com>
2012-02-12 21:13:59 +0100

    Small error correction, the project could not be launched

--- Mobile/src/com/toursims/mobile/HomeActivity.java
+++ Mobile/src/com/toursims/mobile/HomeActivity.java
@@ -1,16 +1,12 @@
 package com.toursims.mobile;
 
-
-import android.app.ListActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
 
-public class HomeActivity extends ListActivity {
+public class HomeActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
