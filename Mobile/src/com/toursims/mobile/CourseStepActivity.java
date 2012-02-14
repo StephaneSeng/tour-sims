52d41fcb604c63ebb839a976cc28052120bbca76
Your Name <bibou@ubuntu.(none)>
2012-02-14 22:31:36 +0100

    Fonctionne jusqu'à la carte + charge placemarks + corriger bug de la webview

--- Mobile/src/com/toursims/mobile/CourseStepActivity.java
+++ Mobile/src/com/toursims/mobile/CourseStepActivity.java
@@ -10,6 +10,7 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
+import com.toursims.mobile.controller.CourseBDD;
 import com.toursims.mobile.model.kml.Placemark;
 import com.toursims.mobile.model.kml.Point;
 import com.toursims.mobile.ui.utils.CustomItemizedOverlay;
@@ -56,12 +57,15 @@ public class CourseStepActivity extends MapActivity{
     }
     
     protected List<Placemark> getPlaceMarks() {
-    	List<Placemark> l = new ArrayList<Placemark>();
-    	for (int i = 0; i< 10; i++) {
-    		Placemark p = new Placemark();
-    		p.setPoint(new Point());
-    		l.add(p);
-    	}
+        Bundle bundle = getIntent().getExtras();
+        int course_id = bundle.getInt("COURSE_ID");
+        
+        CourseBDD datasource = new CourseBDD(this);
+    	datasource.open();
+    	   	
+    	List<Placemark> l = datasource.getAllPlacemarksWithCourseId(course_id);
+    	
+    	datasource.close();
     	
     	return l;
     }
