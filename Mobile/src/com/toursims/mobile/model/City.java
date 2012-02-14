5347daef1e33605a0ba040fc8f130332ed116526
bibou <bibou@ubuntu.(none)>
2012-02-14 10:37:43 +0100

    Add City

---
+++ Mobile/src/com/toursims/mobile/model/City.java
@@ -0,0 +1,43 @@
+package com.toursims.mobile.model;
+import java.util.List;
+
+public class City {
+
+	private String name; 
+	private String coverPictureURL;
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCoverPictureURL() {
+		return coverPictureURL;
+	}
+	
+	public void setCoverPictureURL(String coverPictureURL) {
+		this.coverPictureURL = coverPictureURL;
+	}
+	
+
+
+
+
+
+
+
+
+
+}
