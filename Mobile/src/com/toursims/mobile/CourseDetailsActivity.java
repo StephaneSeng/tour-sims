41b8d505e5d886501d1ef0b40958cce2037d9b31
Nam <hoainam87@gmail.com>
2012-02-27 14:33:02 +0100

    Modified interface details course : added a tab for course step.

--- Mobile/src/com/toursims/mobile/CourseDetailsActivity.java
+++ Mobile/src/com/toursims/mobile/CourseDetailsActivity.java
@@ -11,26 +11,29 @@ public class CourseDetailsActivity extends TabActivity{
         setContentView(R.layout.coursedetails);
         
         //Get course name send by parent Activity
-        Intent i = getIntent();
-        String courseName = i.getStringExtra("coursename");
+        //Intent i = getIntent();
+        //String courseName = i.getStringExtra("coursename");
  
         //Set title of current screen to course name
-        setTitle(courseName);
+        //setTitle(courseName);
                
         TabHost tabHost = getTabHost();  // The activity TabHost
         TabHost.TabSpec spec;  // Resusable TabSpec for each tab
         Intent intent;  // Reusable Intent for each tab
 
-        // Create an Intent to launch an Activity for the tab (to be reused)
-        intent = new Intent().setClass(this, MapActivity.class);
-
         // Initialize a TabSpec for each tab and add it to the TabHost
+        // Tab Map
+        Bundle bundle = getIntent().getExtras();
+        int course_id = bundle.getInt("COURSE_ID");
+        intent = new Intent().setClass(this, CourseStepActivity.class);
+        intent.putExtra("COURSE_ID", course_id);
         spec = tabHost.newTabSpec("map").setIndicator("Map")
                       .setContent(intent);
         tabHost.addTab(spec);
 
         // Tab Step
-        intent = new Intent().setClass(this, CourseStepActivity.class);
+        intent = new Intent().setClass(this, CourseMapActivity.class);
+        
         spec = tabHost.newTabSpec("step").setIndicator("Step")
                       .setContent(intent);
         tabHost.addTab(spec);
--- Mobile/src/com/toursims/mobile/CourseMapActivity.java
+++ Mobile/src/com/toursims/mobile/CourseMapActivity.java
@@ -3,7 +3,7 @@ package com.toursims.mobile;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MapActivity extends Activity{
+public class CourseMapActivity extends Activity{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
