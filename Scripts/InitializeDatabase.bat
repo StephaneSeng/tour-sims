01d72769701776050afaef9a5b9e2392a2d061a2
Stephane <seng.stephane@gmail.com>
2012-02-28 10:10:14 +0100

    Renamed a file for coherence

--- Scripts/InitializeDatabase.bat
+++ Scripts/InitializeDatabase.bat
@@ -3,7 +3,7 @@ REM The bin directory from your PostgreSQL installation should be in your curren
 psql -h localhost -p 5432 -U postgres -f ./CreateDatabase.sql
 psql -h localhost -p 5432 -d toursims_debug -U postgres -f ./CreateSchema.sql
 psql -h localhost -p 5432 -d toursims_debug -U postgres -f ./CreateTablesUser.sql
-psql -h localhost -p 5432 -d toursims_debug -U postgres -f ./CreateTablesCoursePOI.sql
+psql -h localhost -p 5432 -d toursims_debug -U postgres -f ./CreateTablesCityCourse.sql
 psql -h localhost -p 5432 -d toursims_debug -U postgres -f ./CreateTablesStepTrial.sql
 
 PAUSE
