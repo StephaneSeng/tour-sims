baa8a6177ee085aca5566192a5598c649f70c92e
Quentin Villers <quentin.villers@gmail.com>
2012-03-05 14:15:32 +0100

    ...

--- Mobile/src/com/toursims/mobile/CourseStepActivity.java
+++ Mobile/src/com/toursims/mobile/CourseStepActivity.java
@@ -76,7 +76,7 @@ public class CourseStepActivity extends MapActivity{
 	private static final String PROXIMITY_RECEIVER = LocalizationService.class.getName()+".PROXIMITY_RECEIVER";
 	private static final long MINIMUM_DISTANCECHANGE_FOR_UPDATE = 1; // in Meters
 	private static final long MINIMUM_TIME_BETWEEN_UPDATE = 3000; // in Milliseconds
-    private static final long POINT_RADIUS = 10; // in Meters
+    private static final long POINT_RADIUS = 25; // in Meters
     private static final long PROX_ALERT_EXPIRATION = -1;    
 
 	private static LocalizationService serviceLocalization;
@@ -555,8 +555,7 @@ class MapOverlay extends com.google.android.maps.Overlay {
                     y1 = y2;
             }
     }
-    
-
+        
 }
 
 
