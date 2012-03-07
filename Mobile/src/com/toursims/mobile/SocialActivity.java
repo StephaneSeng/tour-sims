efd6ce9c250568b6770570b5bbfa6483de0981cd
unknown <think@think-THINK.(none)>
2012-03-07 09:27:49 +0100

    list de commentaires cree

--- Mobile/src/com/toursims/mobile/SocialActivity.java
+++ Mobile/src/com/toursims/mobile/SocialActivity.java
@@ -3,9 +3,12 @@ package com.toursims.mobile;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SocialActivity extends Activity {
+public class SocialActivity extends Activity{
+	
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+	public void onCreate(Bundle savedInstantState){
+		super.onCreate(savedInstantState);
+		setContentView(R.layout.social);
+	}
+
 }
