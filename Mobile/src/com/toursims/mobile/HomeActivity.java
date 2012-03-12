cf40b42dfcdaaad4f45614a0fca41993f3879cf6
Quentin Villers <quentin.villers@gmail.com>
2012-03-12 10:54:25 +0100

    add files

--- Mobile/src/com/toursims/mobile/HomeActivity.java
+++ Mobile/src/com/toursims/mobile/HomeActivity.java
@@ -34,14 +34,14 @@ import com.toursims.mobile.model.User;
 import com.toursims.mobile.model.kml.Point;
 
 public class HomeActivity extends Activity {
-	
+
 	public static final String ALREADY_ASKED_TO_RESUME = "already_asked_to_resume";
-		
+
 	/**
 	 * Android debugging tag
 	 */
 	@SuppressWarnings("unused")
-	private static final String TAG = HomeActivity.class.getName(); 
+	private static final String TAG = HomeActivity.class.getName();
 	private static SharedPreferences settings;
 	private static SharedPreferences.Editor editor;
 
@@ -53,7 +53,7 @@ public class HomeActivity extends Activity {
 	private ListView lv2;
 	private ImageView recImage;
 	private EditText e;
-	
+
 	private LocalizationService s;
 
 	/**
@@ -62,248 +62,275 @@ public class HomeActivity extends Activity {
 	TourSims tourSims;
 
 	/** Called when the activity is first created. */
-    @Override 
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        recImage = (ImageView)findViewById(R.id.recImage);
-	    lv = (ListView) findViewById(R.id.lvListe);
-	    lv2 = (ListView) findViewById(R.id.lvListe2);
-                
-    	settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0); 
-		settings.edit()
-			.remove(ALREADY_ASKED_TO_RESUME)
-			.commit();
-        
-        // Application context initialisation
-        tourSims = (TourSims) getApplication();
-        
-        //----------------------------------------------------
-	    // HOME
-	    //----------------------------------------------------
-        
-        List<HomeItem> items = new ArrayList<HomeItem>();
-        	           
-        items.add(new HomeItem(R.string.home_cities_all, R.drawable.ic_menu_compass,CityActivity.class));
-        items.add(new HomeItem(R.string.home_poi, R.drawable.ic_menu_info_details,POIActivity.class));
-        
-    	settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0); 
-    	if(settings.contains(CustomPreferences.COURSE_STARTED_URL)){
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		recImage = (ImageView) findViewById(R.id.recImage);
+		lv = (ListView) findViewById(R.id.lvListe);
+		lv2 = (ListView) findViewById(R.id.lvListe2);
+
+		settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
+		settings.edit().remove(ALREADY_ASKED_TO_RESUME).commit();
+
+		// Application context initialisation
+		tourSims = (TourSims) getApplication();
+
+		// ----------------------------------------------------
+		// HOME
+		// ----------------------------------------------------
+
+		List<HomeItem> items = new ArrayList<HomeItem>();
+
+		items.add(new HomeItem(R.string.home_cities_all,
+				R.drawable.ic_menu_compass, CityActivity.class));
+		items.add(new HomeItem(R.string.home_poi,
+				R.drawable.ic_menu_info_details, POIActivity.class));
+
+		settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
+		if (settings.contains(CustomPreferences.COURSE_STARTED_URL)) {
 			try {
 				CourseBDD datasource = new CourseBDD(this);
 				datasource.open();
-				Course c = datasource.getCourseWithId(settings.getInt(CustomPreferences.COURSE_STARTED_ID,1));
+				Course c = datasource.getCourseWithId(settings.getInt(
+						CustomPreferences.COURSE_STARTED_ID, 1));
 				datasource.close();
-				
-				if(c.getCoverPictureURL()!=null){
-					items.add(new HomeItem(R.string.home_goon_course, c.getCoverPictureURL(), new OnClickListener(){
-						public void onClick(View v) {
-							restartCourse();
-						}
-		    		}));
-				} else {
-					items.add(new HomeItem(R.string.home_goon_course, R.drawable.ic_menu_myplaces, new OnClickListener(){
-						public void onClick(View v) {
-							restartCourse();
-						}
-		    		}));
-				}
-		   		
-			} catch (IOException e) {
-			e.printStackTrace();
-			}
- 
-    	}      
-        items.add(new HomeItem(R.string.home_my_records,R.drawable.ic_menu_mylocation,TracesActivity.class));
-              
-	    adapter = new HomeAdapter(this, items,getCacheDir().getAbsolutePath());
-	    lv.setAdapter(adapter);
-	    
-	    ToolBox.setListViewHeightBasedOnChildren(lv);	            
-	    //----------------------------------------------------
-	    // SOCIAL
-	    //----------------------------------------------------
-	            
-	    items2.add(new HomeItem(R.string.home_social_map, R.drawable.ic_menu_globe,SocialActivity.class));   
-	    items2.add(new HomeItem(R.string.home_social_chat, R.drawable.ic_menu_dialog,ChatActivity.class));
-        items2.add(new HomeItem(R.string.home_social_contacts, R.drawable.ic_menu_allfriends,ContactActivity.class));  
-        items2.add(new HomeItem(R.string.home_social_profile, R.drawable.ic_menu_user, new OnClickListener(){
-        	public void onClick(View v) {
-    			Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-    			intent.putExtra(User.USER_ID_EXTRA, tourSims.getUser().getUserId());
-    			startActivity(intent);
-			}
-        }));
-        
-        items2.add(new HomeItem(R.string.home_social_login, R.drawable.ic_menu_user, new OnClickListener(){
-			public void onClick(View v) {
-    			Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-    			startActivityForResult(intent, 0);
-			}
-		}));
-                         
-	    adapter2 = new HomeAdapter(this, items2,getCacheDir().getAbsolutePath());
-	    lv2.setAdapter(adapter2);   	
-	    ToolBox.setListViewHeightBasedOnChildren(lv2);
 
-	    doBindService();
+				if (c.getCoverPictureURL() != null) {
+					items.add(new HomeItem(R.string.home_goon_course, c
+							.getCoverPictureURL(), new OnClickListener() {
+						public void onClick(View v) {
+							restartCourse();
+						}
+					}));
+				} else {
+					items.add(new HomeItem(R.string.home_goon_course,
+							R.drawable.ic_menu_myplaces, new OnClickListener() {
+								public void onClick(View v) {
+									restartCourse();
+								}
+							}));
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+		items.add(new HomeItem(R.string.home_my_records,
+				R.drawable.ic_menu_mylocation, TracesActivity.class));
+
+		adapter = new HomeAdapter(this, items, getCacheDir().getAbsolutePath());
+		lv.setAdapter(adapter);
+
+		ToolBox.setListViewHeightBasedOnChildren(lv);
+		// ----------------------------------------------------
+		// SOCIAL
+		// ----------------------------------------------------
+
+		items2.add(new HomeItem(R.string.home_social_map,
+				R.drawable.ic_menu_globe, SocialActivity.class));
+		items2.add(new HomeItem(R.string.home_social_chat,
+				R.drawable.ic_menu_dialog, ChatActivity.class));
+		items2.add(new HomeItem(R.string.home_social_contacts,
+				R.drawable.ic_menu_allfriends, ContactActivity.class));
+		items2.add(new HomeItem(R.string.home_social_profile,
+				R.drawable.ic_menu_user, new OnClickListener() {
+					public void onClick(View v) {
+						Intent intent = new Intent(getApplicationContext(),
+								ProfileActivity.class);
+						intent.putExtra(User.USER_ID_EXTRA, tourSims.getUser()
+								.getUserId());
+						startActivity(intent);
+					}
+				}));
+
+		items2.add(new HomeItem(R.string.home_social_login,
+				R.drawable.ic_menu_user, new OnClickListener() {
+					public void onClick(View v) {
+						Intent intent = new Intent(getApplicationContext(),
+								LoginActivity.class);
+						startActivityForResult(intent, 0);
+					}
+				}));
+
+		adapter2 = new HomeAdapter(this, items2, getCacheDir()
+				.getAbsolutePath());
+		lv2.setAdapter(adapter2);
+		ToolBox.setListViewHeightBasedOnChildren(lv2);
+
+		doBindService();
 		recImage();
-    }
-    
-    @Override
+	}
+
+	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
 		// User connection management
-//		TextView nameTextView = (TextView)findViewById(R.id.nameTextView);
-//		TextView btnGoogleLogin = (TextView)findViewById(R.id.googleLogin);
-		
+		// TextView nameTextView = (TextView)findViewById(R.id.nameTextView);
+		// TextView btnGoogleLogin = (TextView)findViewById(R.id.googleLogin);
+
 		if (tourSims.isUserLoggedIn()) {
-			items2.remove(items2.size()-1);
+			items2.remove(items2.size() - 1);
 			items2.get(4).setPictureURL(tourSims.getUser().getAvatar());
 			adapter2.setItems(items2);
-		    lv2.setAdapter(adapter2);   	
-		    ToolBox.setListViewHeightBasedOnChildren(lv2);
-			//			nameTextView.setText("Welcome, please login with your Google Account...");
+			lv2.setAdapter(adapter2);
+			ToolBox.setListViewHeightBasedOnChildren(lv2);
+			// nameTextView.setText("Welcome, please login with your Google Account...");
 		} else {
 			// The user is not yet connected
-//			nameTextView.setText("Welcome " + tourSims.getUserName() + " !");
-//			TextView textView = (TextView) findViewById(R.id.home_user_name);
-//			ImageView imageView = (ImageView) findViewById(R.id.home_avatar);
-//			textView.setText(tourSims.getUser().getName());
-//			imageView.setImageBitmap(tourSims.getUser().getAvatarBitmap());
+			// nameTextView.setText("Welcome " + tourSims.getUserName() + " !");
+			// TextView textView = (TextView) findViewById(R.id.home_user_name);
+			// ImageView imageView = (ImageView) findViewById(R.id.home_avatar);
+			// textView.setText(tourSims.getUser().getName());
+			// imageView.setImageBitmap(tourSims.getUser().getAvatarBitmap());
 		}
-		
-        popUpRestart();
-        recImage();
+
+		popUpRestart();
+		recImage();
 	}
-    
-    private void popUpRestart(){
-    	settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0); 
-    	
-    	if(settings.contains(CustomPreferences.COURSE_STARTED_URL)&&!settings.getBoolean(ALREADY_ASKED_TO_RESUME, true)){
-    		    		
+
+	private void popUpRestart() {
+		settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
+
+		if (settings.contains(CustomPreferences.COURSE_STARTED_URL)
+				&& !settings.getBoolean(ALREADY_ASKED_TO_RESUME, true)) {
+
 			AlertDialog.Builder dialog = ToolBox.getDialog(this);
-			
-    		dialog.setTitle(R.string.course_already_started_title)
-				.setMessage(R.string.course_already_started_message)
-				.setPositiveButton(R.string.course_already_started_go_on, new DialogInterface.OnClickListener() {
-					
-				public void onClick(DialogInterface dialog, int which) {	
-						// TODO Auto-generated method stub
-				          restartCourse();
-					}
-				});
-			
-			dialog.setNegativeButton(R.string.course_already_started_discard, new DialogInterface.OnClickListener() {
-				
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO Auto-generated method stub
-					SharedPreferences.Editor editor = settings.edit();
-					
-					for (String item : CustomPreferences.COURSE_ALL) {
-						editor.remove(item)
-							.commit();
-					}
-					
-					items.remove(items.size()-1);
-					adapter.setItems(items);
-					lv.setAdapter(adapter);
-				    ToolBox.setListViewHeightBasedOnChildren(lv);
-					dialog.dismiss();
-				}
-			});
-			
-			dialog.setNeutralButton(R.string.later, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO Auto-generated method stub
-					SharedPreferences.Editor editor = settings.edit();
-					editor.putBoolean(ALREADY_ASKED_TO_RESUME, true)
-						.commit();
-					dialog.dismiss();		
-				}
-			});
+
+			dialog.setTitle(R.string.course_already_started_title)
+					.setMessage(R.string.course_already_started_message)
+					.setPositiveButton(R.string.course_already_started_go_on,
+							new DialogInterface.OnClickListener() {
+
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// TODO Auto-generated method stub
+									restartCourse();
+								}
+							});
+
+			dialog.setNegativeButton(R.string.course_already_started_discard,
+					new DialogInterface.OnClickListener() {
+
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							SharedPreferences.Editor editor = settings.edit();
+
+							for (String item : CustomPreferences.COURSE_ALL) {
+								editor.remove(item).commit();
+							}
+
+							items.remove(items.size() - 1);
+							adapter.setItems(items);
+							lv.setAdapter(adapter);
+							ToolBox.setListViewHeightBasedOnChildren(lv);
+							dialog.dismiss();
+						}
+					});
+
+			dialog.setNeutralButton(R.string.later,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							SharedPreferences.Editor editor = settings.edit();
+							editor.putBoolean(ALREADY_ASKED_TO_RESUME, true)
+									.commit();
+							dialog.dismiss();
+						}
+					});
 			dialog.show();
-    	}   	
-    }
-    
-    private void restartCourse() {
-    	Intent intent = new Intent(getApplicationContext(),CourseStepActivity.class);
-    	
-        intent.putExtra(Course.URL_EXTRA, settings.getString(CustomPreferences.COURSE_STARTED_URL, null))
-        	.putExtra(Course.ID_EXTRA, settings.getInt(CustomPreferences.COURSE_STARTED_ID, 0));
-        
-        startActivity(intent);
-    }
-    
-    @Override
-    protected void onDestroy() {
-    	// TODO Auto-generated method stub
+		}
+	}
+
+	private void restartCourse() {
+		Intent intent = new Intent(getApplicationContext(),
+				CourseStepActivity.class);
+
+		intent.putExtra(Course.URL_EXTRA,
+				settings.getString(CustomPreferences.COURSE_STARTED_URL, null))
+				.putExtra(Course.ID_EXTRA,
+						settings.getInt(CustomPreferences.COURSE_STARTED_ID, 0));
+
+		startActivity(intent);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
 		SharedPreferences.Editor editor = settings.edit();
-		editor.remove(ALREADY_ASKED_TO_RESUME)
-			.commit();
-    	super.onDestroy();
-    }
-                
-    public void rec(View v){
-    	settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
+		editor.remove(ALREADY_ASKED_TO_RESUME).commit();
+		super.onDestroy();
+	}
+
+	public void rec(View v) {
+		settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
 		editor = settings.edit();
 
-		if(settings.getLong(CustomPreferences.RECORDING_RIGHT_NOW, -1)==-1){		
+		if (settings.getLong(CustomPreferences.RECORDING_RIGHT_NOW, -1) == -1) {
 			final AlertDialog.Builder dialog = ToolBox.getDialog(this);
 			dialog.setTitle(R.string.home_record_title);
 
 			e = new EditText(this);
 			e.setHint(R.string.home_record_hint);
-			
+
 			dialog.setView(e)
-				.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						if(s==null){
-							doBindService();
-						}
-						Long l	= Calendar.getInstance().getTimeInMillis();
-				    	settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
-						editor = settings.edit();
-						editor.putLong(CustomPreferences.RECORDING_RIGHT_NOW,l);
-						editor.commit();
-						Log.d("TAG","Start recording"+l);
-						s.startRecording();
-						recImage();
-						dialog.dismiss();
-						Trace item = new Trace();
-						item.setName(e.getText().toString());
-						item.setMillis(l);
-						insertTrace(item);
-				    
-					}
-				})
-				.setNegativeButton(R.string.home_record_cancel, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface arg0, int arg1) {
-						// TODO Auto-generated method stub
-						}
-				})
-			.show();
+					.setPositiveButton(R.string.ok,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int which) {
+									if (s == null) {
+										doBindService();
+									}
+									Long l = Calendar.getInstance()
+											.getTimeInMillis();
+									settings = getSharedPreferences(
+											CustomPreferences.PREF_FILE, 0);
+									editor = settings.edit();
+									editor.putLong(
+											CustomPreferences.RECORDING_RIGHT_NOW,
+											l);
+									editor.commit();
+									Log.d("TAG", "Start recording" + l);
+									s.startRecording();
+									recImage();
+									dialog.dismiss();
+									Trace item = new Trace();
+									item.setName(e.getText().toString());
+									item.setMillis(l);
+									insertTrace(item);
+
+								}
+							})
+					.setNegativeButton(R.string.home_record_cancel,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface arg0,
+										int arg1) {
+									// TODO Auto-generated method stub
+								}
+							}).show();
 		} else {
 			s.stopRecording();
 			editor.remove(CustomPreferences.RECORDING_RIGHT_NOW);
 			editor.commit();
-			Toast.makeText(this, R.string.home_record_stop_recording, Toast.LENGTH_LONG).show();
+			Toast.makeText(this, R.string.home_record_stop_recording,
+					Toast.LENGTH_LONG).show();
 			recImage();
 		}
-    }
-    
-    private void recImage() {
-    	settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
-    	if(settings.getLong(CustomPreferences.RECORDING_RIGHT_NOW, -1)==-1){
-    		recImage.setImageResource(R.drawable.ic_media_play);
-    	} else {
-    		recImage.setImageResource(R.drawable.ic_media_pause);
-    	}
-    }
-    
-    private ServiceConnection mConnection = new ServiceConnection() {
+	}
+
+	private void recImage() {
+		settings = getSharedPreferences(CustomPreferences.PREF_FILE, 0);
+		if (settings.getLong(CustomPreferences.RECORDING_RIGHT_NOW, -1) == -1) {
+			recImage.setImageResource(R.drawable.ic_media_play);
+		} else {
+			recImage.setImageResource(R.drawable.ic_media_pause);
+		}
+	}
+
+	private ServiceConnection mConnection = new ServiceConnection() {
 
 		public void onServiceConnected(ComponentName className, IBinder binder) {
 			s = ((LocalizationService.MyBinder) binder).getService();
@@ -318,35 +345,35 @@ public class HomeActivity extends Activity {
 		bindService(new Intent(this, LocalizationService.class), mConnection,
 				Context.BIND_AUTO_CREATE);
 	}
-	
-	public void insertTrace(Trace item){
+
+	public void insertTrace(Trace item) {
 		try {
 			CourseBDD datasource;
 			datasource = new CourseBDD(this);
 			datasource.open();
 			datasource.insertTrace(item);
-		    datasource.close();
+			datasource.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void setFlag(View v){
-		if(s==null){
+
+	public void setFlag(View v) {
+		if (s == null) {
 			doBindService();
 		}
-		if(s!=null){
-			if(s.getKnownLocation()!=null){
+		if (s != null) {
+			if (s.getKnownLocation() != null) {
 				CourseBDD datasource;
-				
+
 				Point item = new Point();
 				Location l = s.getKnownLocation();
-				
+
 				item.setMillis(l.getTime());
 				item.setLongitude(l.getLongitude());
 				item.setLatitude(l.getLatitude());
-				
+
 				try {
 					datasource = new CourseBDD(this);
 					datasource.open();
@@ -354,14 +381,15 @@ public class HomeActivity extends Activity {
 					datasource.close();
 					Toast.makeText(this, "Flag OK", Toast.LENGTH_LONG).show();
 				} catch (IOException e) {
-				e.printStackTrace();
+					e.printStackTrace();
 				}
 			}
 		}
 	}
-	
-	public void traceMap(View v){
-		Intent intent = new Intent(getApplicationContext(), TraceMapActivity.class);
+
+	public void traceMap(View v) {
+		Intent intent = new Intent(getApplicationContext(),
+				TraceMapActivity.class);
 		startActivity(intent);
 	}
 }
