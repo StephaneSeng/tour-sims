df5b91ece6a2657f0b0c7f486e62d702b58f84c6
Quentin Villers <quentin.villers@gmail.com>
2012-03-20 09:34:37 +0100

    handle flags

---
+++ Mobile/src/com/toursims/mobile/FlagMapActivity.java
@@ -0,0 +1,13 @@
+package com.toursims.mobile;
+
+import com.actionbarsherlock.app.SherlockMapActivity;
+
+public class FlagMapActivity extends SherlockMapActivity{
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
